b640ec1f0e9a2b01ec8e9fe6b3310e8a123f19f3
Vitaliy <itvkip@yandex.ru>
2021-02-09 21:52:38 +0300

    - added combineReducers;

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import './App.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
 class App extends Component {
   render() {
-    const { name, surname, age } = this.props.user
+    const { name } = this.props.user;
+    const { year, photos } = this.props.page;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p className="App-intro">Привет из App, {name} {surname}!</p>
-        <p>Тебе уже {age}?</p>
+        <p className="App-intro">
+          Привет из App, {name}!
+        </p>
+        <p>У тебя {photos.length} фото за {year} год.</p>
       </div>
-    )
+    );
   }
 }
 
 // приклеиваем данные из store
-const mapStateToProps = store => {
-  console.log(store) // посмотрим, что же у нас в store?
+const mapStateToProps = (store) => {
+  console.log(store); // посмотрим, что же у нас в store?
   return {
     user: store.user,
-  }
-}
+    page: store.page,
+  };
+};
 
 // в наш компонент App, с помощью connect(mapStateToProps)
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps)(App);
